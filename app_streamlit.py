ffe94a539b920e884be1a2e44fdf22487d771373
macdi011 <159440242+macdi011@users.noreply.github.com>
2024-07-26 22:41:21 +0000

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -1,82 +1,197 @@
-import requests
-import base64
-
-import matplotlib.pyplot as plt
-import numpy as np
-import seaborn as sns
 import streamlit as st
-from PIL import Image
+import pandas as pd
 
-def get_token(clientId,clientSecret):
-    url = "https://accounts.spotify.com/api/token"
-    headers = {}
-    data = {}
-    message = f"{clientId}:{clientSecret}"
-    messageBytes = message.encode('ascii')
-    base64Bytes = base64.b64encode(messageBytes)
-    base64Message = base64Bytes.decode('ascii')
-    headers['Authorization'] = "Basic " + base64Message
-    data['grant_type'] = "client_credentials"
-    r = requests.post(url, headers=headers, data=data)
-    token = r.json()['access_token']
-    return token
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
+import polarplot
+import songrecommendations
 
-def get_track_recommendations(seed_tracks,token):
-    limit = 10
-    recUrl = f"https://api.spotify.com/v1/recommendations?limit={limit}&seed_tracks={seed_tracks}"
+SPOTIPY_CLIENT_ID = 'e18fafeb60a949d2a9b7d1efccabe69a'
+SPOTIPY_CLIENT_SECRET = '739bbbed49864382a64a64ccd64ecdcc'
 
-    headers = {
-        "Authorization": "Bearer " + token
-    }
+auth_manager = SpotifyClientCredentials(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET)
+sp = spotipy.Spotify(auth_manager=auth_manager)
 
-    res = requests.get(url=recUrl, headers=headers)
-    return res.json()
+st.header('Rockstar Spotify App (Streamlit)')
 
-def song_recommendation_vis(reco_df):    
-    reco_df['duration_min'] = round(reco_df['duration_ms'] / 1000, 0)
-    reco_df["popularity_range"] = reco_df["popularity"] - (reco_df['popularity'].min() - 1)
-    
-    plt.figure(figsize=(15, 6), facecolor=(.9, .9, .9))    
+search_choices = ['Song/Track', 'Artist', 'Album']
+search_selected = st.sidebar.selectbox("Your search choice please: ", search_choices)
 
-    x = reco_df['name']
-    y = reco_df['duration_min']
-    s = reco_df['popularity_range']*20
-        
-    color_labels = reco_df['explicit'].unique()
-    rgb_values = sns.color_palette("Set1", 8)
-    color_map = dict(zip(color_labels, rgb_values))
+search_keyword = st.text_input(search_selected + " (Keyword Search)")
+button_clicked = st.button("Search")
 
-    plt.scatter(x, y, s, alpha=0.7, c=reco_df['explicit'].map(color_map))
-    plt.xticks(rotation=90)
-    plt.legend()
-    # show the graph
-    plt.show()
-    
-    st.pyplot(plt)
-    
+search_results = []
+tracks = []
+artists = []
+albums = []
 
-def save_album_image(img_url, track_id):
-    try:
-        # Vérifier si le répertoire existe, sinon le créer
-        if not os.path.exists('img'):
-            os.makedirs('img')
+if search_keyword is not None and len(str(search_keyword)) > 0:
+    if search_selected == 'Song/Track':
+        st.write("Start song/track search")
+        tracks = sp.search(q='track:' + search_keyword, type='track', limit=20)
+        tracks_list = tracks['tracks']['items']
+        if len(tracks_list) > 0:
+            for track in tracks_list:
+                search_results.append(track['name'] + " - By - " + track['artists'][0]['name'])
 
-        # Récupérer l'image depuis l'URL
-        r = requests.get(img_url)
+    elif search_selected == 'Artist':
+        st.write("Start artist search")
+        artists = sp.search(q='artist:' + search_keyword, type='artist', limit=20)
+        artists_list = artists['artists']['items']
+        if len(artists_list) > 0:
+            for artist in artists_list:
+                search_results.append(artist['name'])
 
-        # Enregistrer l'image dans le répertoire img/
-        with open(f'img/{track_id}.jpg', 'wb') as f:
-            f.write(r.content)
+    if search_selected == 'Album':
+        st.write("Start album search")
+        albums = sp.search(q='album:' + search_keyword, type='album', limit=20)
+        albums_list = albums['albums']['items']
+        if len(albums_list) > 0:
+            for album in albums_list:
+                search_results.append(album['name'] + " - By - " + album['artists'][0]['name'])
 
-    except Exception as e:
-        print(f"Error saving album image: {e}")
+selected_album = None
+selected_artist = None
+selected_track = None
 
-def get_album_image(track_id):
-    image_path = f'img/{track_id}.jpg'
-    if os.path.exists(image_path):
-        with open(image_path, 'rb') as f:
-            image = f.read()
-        return image
+if search_selected == 'Song/Track':
+    selected_track = st.selectbox("Select your song/track: ", search_results)
+elif search_selected == 'Artist':
+    selected_artist = st.selectbox("Select your artist: ", search_results)
+elif search_selected == 'Album':
+    selected_album = st.selectbox("Select your album: ", search_results)
+
+if selected_track is not None and len(tracks) > 0:
+    tracks_list = tracks['tracks']['items']
+    track_id = None
+    if len(tracks_list) > 0:
+        for track in tracks_list:
+            str_temp = track['name'] + " - By - " + track['artists'][0]['name']
+            if str_temp == selected_track:
+                track_id = track['id']
+                track_album = track['album']['name']
+                img_album = track['album']['images'][1]['url']
+                songrecommendations.save_album_image(img_album, track_id)
+
+    selected_track_choice = None
+    if track_id is not None:
+        image = songrecommendations.get_album_image(track_id)  # Correction ici
+        st.image(image)
+        track_choices = ['Song Features', 'Similar Songs Recommendation']
+        selected_track_choice = st.sidebar.selectbox('Please select track choice: ', track_choices)
+        if selected_track_choice == 'Song Features':
+            track_features = sp.audio_features(track_id)
+            df = pd.DataFrame(track_features, index=[0])
+            df_features = df.loc[:, ['acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'speechiness', 'valence']]
+            st.dataframe(df_features)
+            polarplot.feature_plot(df_features)
+        elif selected_track_choice == 'Similar Songs Recommendation':
+            token = songrecommendations.get_token(SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET)
+            similar_songs_json = songrecommendations.get_track_recommendations(track_id, token)
+            recommendation_list = similar_songs_json['tracks']
+            recommendation_list_df = pd.DataFrame(recommendation_list)
+            recommendation_df = recommendation_list_df[['name', 'explicit', 'duration_ms', 'popularity']]
+            st.dataframe(recommendation_df)
+            songrecommendations.song_recommendation_vis(recommendation_df)
+
     else:
-        return None
+        st.write("Please select a track from the list")
+
+elif selected_album is not None and len(albums) > 0:
+    albums_list = albums['albums']['items']
+    album_id = None
+    album_uri = None
+    album_name = None
+    if len(albums_list) > 0:
+        for album in albums_list:
+            str_temp = album['name'] + " - By - " + album['artists'][0]['name']
+            if selected_album == str_temp:
+                album_id = album['id']
+                album_uri = album['uri']
+                album_name = album['name']
+    if album_id is not None and album_uri is not None:
+        st.write("Collecting all the tracks for the album :" + album_name)
+        album_tracks = sp.album_tracks(album_id)
+        df_album_tracks = pd.DataFrame(album_tracks['items'])
+        for idx in df_album_tracks.index:
+            with st.container():
+                col1, col2, col3, col4 = st.columns((4, 4, 2, 2))
+                col11, col12 = st.columns((8, 2))
+                col1.write(df_album_tracks['id'][idx])
+                col2.write(df_album_tracks['name'][idx])
+                col3.write(df_album_tracks['duration_ms'][idx])
+                col4.write(df_album_tracks['explicit'][idx])
+                if df_album_tracks['preview_url'][idx] is not None:
+                    col11.write(df_album_tracks['preview_url'][idx])
+                    with col12:
+                        st.audio(df_album_tracks['preview_url'][idx], format="audio/mp3")
+
+if selected_artist is not None and len(artists) > 0:
+    artists_list = artists['artists']['items']
+    artist_id = None
+    artist_uri = None
+    selected_artist_choice = None
+    if len(artists_list) > 0:
+        for artist in artists_list:
+            if selected_artist == artist['name']:
+                artist_id = artist['id']
+                artist_uri = artist['uri']
+
+    if artist_id is not None:
+        artist_choice = ['Albums', 'Top Songs']
+        selected_artist_choice = st.sidebar.selectbox('Select artist choice', artist_choice)
+
+    if selected_artist_choice is not None:
+        if selected_artist_choice == 'Albums':
+            artist_uri = 'spotify:artist:' + artist_id
+            album_result = sp.artist_albums(artist_uri, album_type='album')
+            all_albums = album_result['items']
+            col1, col2, col3 = st.columns((6, 4, 2))
+            for album in all_albums:
+                col1.write(album['name'])
+                col2.write(album['release_date'])
+                col3.write(album['total_tracks'])
+        elif selected_artist_choice == 'Top Songs':
+            artist_uri = 'spotify:artist:' + artist_id
+            top_songs_result = sp.artist_top_tracks(artist_uri)
+            for track in top_songs_result['tracks']:
+                with st.container():
+                    col1, col2, col3, col4 = st.columns((4, 4, 2, 2))
+                    col11, col12 = st.columns((10, 2))
+                    col21, col22 = st.columns((11, 1))
+                    col31, col32 = st.columns((11, 1))
+                    col1.write(track['id'])
+                    col2.write(track['name'])
+                    if track['preview_url'] is not None:
+                        col11.write(track['preview_url'])
+                        with col12:
+                            st.audio(track['preview_url'], format="audio/mp3")
+
+                    with col3:
+                        def feature_requested():
+                            track_features = sp.audio_features(track['id'])
+                            df = pd.DataFrame(track_features, index=[0])
+                            df_features = df.loc[:, ['acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'speechiness', 'valence']]
+                            with col21:
+                                st.dataframe(df_features)
+                            with col31:
+                                polarplot.feature_plot(df_features)
+
+                        feature_button_state = st.button('Track Audio Features', key=track['id'], on_click=feature_requested)
+
+                    with col4:
+                        def similar_songs_requested():
+                            token = songrecommendations.get_token(SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET)
+                            similar_songs_json = songrecommendations.get_track_recommendations(track['id'], token)
+                            recommendation_list = similar_songs_json['tracks']
+                            recommendation_list_df = pd.DataFrame(recommendation_list)
+                            recommendation_df = recommendation_list_df[['name', 'explicit', 'duration_ms', 'popularity']]
+                            with col21:
+                                st.dataframe(recommendation_df)
+                            with col31:
+                                songrecommendations.song_recommendation_vis(recommendation_df)
+
+                        similar_songs_state = st.button('Similar Songs', key=track['id'], on_click=similar_songs_requested)
+
+                    st.write('----')
